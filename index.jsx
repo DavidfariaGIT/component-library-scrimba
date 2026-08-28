5dd7cbc30d67d79f6c3dbba5e594031d87fa3d3c
DavidfariaGIT <davidfaria17@gmail.com>
2026-08-28 18:54:36 -0400

    banner pt 1

--- index.jsx
+++ index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { loremIpsum, LoremIpsum } from 'lorem-ipsum';
 import Badge from './components/Badge';
+import Banner from './components/Banner';
+
 
     
 
@@ -15,6 +18,11 @@ function App() {
       {return <Badge key={color} backgroundColor={color} variant="pill">Badge</Badge>}
     })
 
+    const goodIcon = <img className="icon" src="./icons/good.png"/>
+    const warningIcon = <img className="icon" src="./icons/warning.png"/>
+    const dangerIcon = <img className="icon"src="./icons/danger.png"/>
+    const infoIcon = <img className="icon" src="./icons/info.png"/>
+
   return (
     <main>
     <div className='badge-container'>
@@ -25,6 +33,13 @@ function App() {
     <p><b>Pill</b></p>
     {badgesPill}
     </div>
+
+    <section className='banner-container'>
+    <Banner variant="success" message={loremIpsum()}>{goodIcon} Congrats</Banner>
+    <Banner variant="warning" message={loremIpsum()}>{warningIcon} Attention</Banner>
+    <Banner variant="error" message={loremIpsum()}>{dangerIcon} There is a problem with your app</Banner>
+    <Banner variant="neutral" message={loremIpsum()}>{infoIcon} Update avaliable </Banner>
+    </section>
     </main>
   )
 }
